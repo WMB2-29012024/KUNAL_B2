d41718bf703349395606a5b104f55bee858c2448
Kunalkumar9264 <kunalchoudhary@gmail.com>
2024-02-21 17:01:29 +0530

    Q1 primitivedata

---
+++ question.js
@@ -0,0 +1,8 @@
+1.  //Create an array/list of names of all primitive data types in javascript.
+
+   // List of all primitive data type
+
+    const primitiveDatatype=["String","number", "Boolean","null","Undefined","Symbol","Bigint"];
+    console.log(primitiveDatatype);
+
+   
